835c4f96644cd52a06752b75177f61ffdae278f2
Henrique Poggi <henriquep.r@hotmail.com>
2021-12-26 00:48:55 -0300

    Criacao do repositorio

---
+++ src/main/java/com/pweb2/kadernotas/repository/AdministradorRepository.java
@@ -0,0 +1,9 @@
+package com.pweb2.kadernotas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pweb2.kadernotas.models.Administrador;
+
+public interface AdministradorRepository extends JpaRepository<Administrador, Long>{
+
+}
---
+++ src/main/java/com/pweb2/kadernotas/repository/EstudanteRepository.java
@@ -0,0 +1,9 @@
+package com.pweb2.kadernotas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pweb2.kadernotas.models.Estudante;
+
+public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
+
+}
